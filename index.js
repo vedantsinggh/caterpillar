33266bf5b05fcf3d134ed02868e7f973a5be291e
vedantsinggh <vedantsinggh@gmail.com>
2024-08-09 22:46:16 +0530

    added distance constrained for 1 blob

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("board");
 const ctx    = canvas.getContext("2d");
+canvas.width  = 800;
+canvas.height = 600;
+
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
@@ -8,42 +11,39 @@ function getMousePos(canvas, evt) {
   };
 }
 
-canvas.width  = 800;
-canvas.height = 600;
-
 ctx.fillStyle = "rgb(40 40 40)";
 ctx.fillRect(0,0,800,600);
 
-const dx = 100;
-const dy = 100;
-const radius = 20;
-let x = 0, y = 0;
-let x2 = 0, y2 = 0;
-
-let tarX, tarY;
-
-const drawCircle = (ctx, x, y, radius) => {
+const drawCircle = (ctx, x, y, radius, color) => {
 	ctx.beginPath();
 	ctx.arc(x, y, radius, 0, Math.PI * 2);
-	ctx.fillStyle = "rgb(224 183 247)";
+	ctx.fillStyle = color;
 	ctx.fill()
 	ctx.strokeStyle = "rgb(179 52 250)";
 	ctx.lineWidth = 4;
 	ctx.stroke();
 };
 
-const drawSnake = (ctx, x,y,radius) => {
-	drawCircle(ctx,x,y,radius)
-
-	drawCircle(ctx,x2,y2,radius)
+const drawSnake = (ctx, x,y,x2,y2,radius) => {
+	drawCircle(ctx,x,y,radius,"rgb(224 183 247)")
+	drawCircle(ctx,x2,y2,radius, "rgb(224 183 247)");
 };
 
-
+const dx = 100;
+const dy = 100;
+const radius = 20;
+let x = 0, y = 0;
+let x2 = 2, y2 = 2;
+let tarX, tarY;
 
 let start;
 function step(timeStamp){
 	if(start === undefined){
 		start = timeStamp;
+	}
+	
+	if(x2 === undefined){
+		console.log("ss")
 	}
 
 	const dt = 0.001 * (timeStamp - start);
@@ -69,7 +69,15 @@ function step(timeStamp){
 
 	ctx.fillStyle = "rgb(40 40 40)";
 	ctx.fillRect(0,0,800,600);
-	drawSnake(ctx,x,y,radius)
+
+	const dirX = x - x2;
+	const dirY = y - y2;
+	const mag  = Math.sqrt(Math.pow(dirX,2) + Math.pow(dirY,2))
+
+	x2 = x - (dirX / mag) * radius;
+	y2 = y - (dirY / mag) * radius;
+
+	drawSnake(ctx,x,y,x2,y2,radius)
 
 	window.requestAnimationFrame(step);
 }
